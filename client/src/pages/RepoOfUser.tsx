4b36d1907df908076c7f806f9901f383f377cfcd
magley <60552482+magley@users.noreply.github.com>
2024-12-17 19:00:59 +0100

    feat: Add client-side search+filter for `get repos of user`
    It's client-side because 1) it's easier and 2) we can assume that one user will never have so many repositories that we'd need to paginate and implement filtering server-side. I say <100 repos per user is _fine_. No need to future-proof.
    
    Just in case, we can first cache and only later change the architecture and API.

--- client/src/pages/RepoOfUser.tsx
+++ client/src/pages/RepoOfUser.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { Card, Row, Col, Spinner } from 'react-bootstrap';
+import { Card, Row, Col, Spinner, Button } from 'react-bootstrap';
 import { RepoDTO, RepositoryService, ReposOfUserDTO } from '../api/repo.api';
 import { AxiosError, AxiosResponse } from 'axios';
 import './RepoOfUser.css';
@@ -8,12 +8,20 @@ import { getJwtId } from '../util/localstorage';
 
 export const RepositoriesOfUser: React.FC = () => {
     const [fullResult, setFullResult] = useState<ReposOfUserDTO>();
-    const [repositories, setRepositories] = useState<RepoDTO[]>([]);
     const [orgNames, setOrgNames] = useState<Map<number, string>>();
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
     const { userid } = useParams<{ userid: string }>();
     const [myId, setMyId] = useState<number>();
+
+    const [repositories, setRepositories] = useState<RepoDTO[]>([]);
+    const [filteredRepos, setFilteredRepos] = useState<RepoDTO[]>([]);
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showAdvancedSearch, setShowAdvancedSearch] = useState(false);
+    const [selectedOrg, setSelectedOrg] = useState<number | undefined>(undefined);
+    const [showPublic, setShowPublic] = useState(true);
+    const [showPrivate, setShowPrivate] = useState(true);
 
     useEffect(() => {
         fetchRepos();
@@ -30,17 +38,50 @@ export const RepositoriesOfUser: React.FC = () => {
             const orgNamesMap = new Map(Object.entries(res.data.organization_names).map(([key, value]) => [Number(key), value]));
             setOrgNames(orgNamesMap);
 
-            console.log(orgNames);
-            console.log(res.data.organization_names);
-            console.log(typeof (res.data.organization_names));
-
-            console.log(res.data);
+            setRepositories(res.data.repos);
+            setFilteredRepos(res.data.repos);
         }).catch((err: AxiosError) => {
             setError(`${err}`);
-
-            console.error(err);
         })
     }
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+        filterRepos(e.target.value, selectedOrg, showPublic, showPrivate);
+    };
+
+    const toggleAdvancedSearch = () => {
+        setShowAdvancedSearch(!showAdvancedSearch);
+    };
+
+    const handleOrgChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const orgId = Number(e.target.value);
+        setSelectedOrg(orgId);
+        filterRepos(searchTerm, orgId, showPublic, showPrivate);
+    };
+
+    const handlePublicChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setShowPublic(e.target.checked);
+        filterRepos(searchTerm, selectedOrg, e.target.checked, showPrivate);
+    };
+
+    const handlePrivateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setShowPrivate(e.target.checked);
+        filterRepos(searchTerm, selectedOrg, showPublic, e.target.checked);
+    };
+
+    const filterRepos = (searchTerm: string, orgId?: number, showPublic?: boolean, showPrivate?: boolean) => {
+        let filtered = repositories.filter((repo) => {
+            const matchesSearch =
+                repo.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                (repo.desc && repo.desc.toLowerCase().includes(searchTerm.toLowerCase()));
+            const matchesOrg = orgId ? repo.organization_id === orgId : true;
+            const matchesVisibility = (showPublic && repo.public) || (showPrivate && !repo.public);
+            return matchesSearch && matchesOrg && matchesVisibility;
+        });
+        setFilteredRepos(filtered);
+    };
+
 
     if (loading) {
         return (
@@ -56,9 +97,74 @@ export const RepositoriesOfUser: React.FC = () => {
 
     return (
         <Row className="g-4 repo-of-user">
+            {/* Page Title */}
             {userid == myId ? (<h1>Your repositories</h1>) : (<h1>{fullResult!.user_name}'s repositories</h1>)}
 
-            {repositories.map((repo) => (
+            <div className="d-flex justify-content-between">
+                {/* Search Bar */}
+                <input
+                    type="text"
+                    className="form-control me-2"
+                    placeholder="Search repositories"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                {/* Advanced Search Button */}
+                <Button className="btn btn-primary" onClick={toggleAdvancedSearch}>
+                    {showAdvancedSearch ? <i className="bi bi-funnel-fill"></i> : <i className="bi bi-funnel"></i>}
+                </Button>
+            </div>
+
+            {/* Advanced Search Section */}
+            {showAdvancedSearch && (
+                <div className="advanced-search">
+                    <div className="mb-1">
+                        <select
+                            id="orgSelect"
+                            className="form-select"
+                            value={selectedOrg || ''}
+                            onChange={handleOrgChange}
+                        >
+                            <option value="">All Organizations</option>
+                            {Array.from(orgNames!.entries()).map(([id, name]) => (
+                                <option key={id} value={id}>
+                                    {name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    {/* Visibility Filter - Public/Private Checkboxes */}
+                    <div className="mb-3">
+                        <div className="form-check ms-2 d-flex align-items-center">
+                            <input
+                                type="checkbox"
+                                className="form-check-input form-check-lg"
+                                checked={showPublic}
+                                onChange={handlePublicChange}
+                                id="publicCheckbox"
+                            />
+                            <label className="form-check-label fs-5 ms-2" htmlFor="publicCheckbox">
+                                <i className="bi bi-journal-bookmark"></i> <b>Public</b>
+                            </label>
+                        </div>
+                        <div className="form-check ms-2 d-flex align-items-center">
+                            <input
+                                type="checkbox"
+                                className="form-check-input form-check-lg"
+                                checked={showPrivate}
+                                onChange={handlePrivateChange}
+                                id="privateCheckbox"
+                            />
+                            <label className="form-check-label fs-5 ms-2" htmlFor="privateCheckbox">
+                                <i className="bi bi-lock"></i> <b>Private</b>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {filteredRepos.map((repo) => (
                 <Col key={repo.id} xs={12}>
                     <Card>
                         <Card.Body>
