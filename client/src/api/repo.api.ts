863bc2acd4a9bbd31c0253690762bd9f5af918d6
magley <60552482+magley@users.noreply.github.com>
2024-12-19 21:38:52 +0100

    feat: Show badge (+filter), downloads and last modified in user repo page

--- client/src/api/repo.api.ts
+++ client/src/api/repo.api.ts
@@ -1,6 +1,13 @@
 import { AxiosResponse } from "axios";
 import { axiosInstance } from "../util/http";
 
+export enum RepositoryBadge {
+    none = "none",
+    official = "official",
+    verified = "verified",
+    sponsored_oss = "sponsored_oss",
+}
+
 export interface RepoCreateDTO {
     name: string,
     desc: string,
@@ -14,9 +21,11 @@ export interface RepoDTO {
     canonical_name: string,
     desc: string
     public: boolean,
-    official: boolean,
     owner_id: number,
     organization_id: number | null,
+    badge: RepositoryBadge,
+    last_updated: string, // Encoded Date() object.
+    downloads: number,
 }
 
 export interface ReposOfUserDTO {
@@ -32,6 +41,40 @@ export interface RepoExtDTO extends RepoDTO {
 }
 
 export class RepositoryService {
+    static BadgeToHumanText(badge: RepositoryBadge): string {
+        switch (badge) {
+            case RepositoryBadge.none: return "";
+            case RepositoryBadge.official: return "Official";
+            case RepositoryBadge.verified: return "Verified Publisher";
+            case RepositoryBadge.sponsored_oss: return "Sponsored OSS";
+            default: return `${badge}`;
+        }
+    }
+
+    static BadgeToBootstrapColor(badge: RepositoryBadge): string {
+        switch (badge) {
+            case RepositoryBadge.none: return "bg-light";
+            case RepositoryBadge.official: return "bg-primary";
+            case RepositoryBadge.verified: return "bg-secondary";
+            case RepositoryBadge.sponsored_oss: return "bg-success";
+            default: return `bg-light`;
+        }
+    }
+
+    /**
+     * 
+     * Usage: <i className={`bi ${BadgeToBootstrapIcon(...)}`}></i>
+     */
+    static BadgeToHumanBootstrapIcon(badge: RepositoryBadge): string {
+        switch (badge) {
+            case RepositoryBadge.none: return "";
+            case RepositoryBadge.official: return "bi-award";
+            case RepositoryBadge.verified: return "bi-patch-check-fill";
+            case RepositoryBadge.sponsored_oss: return "bi-git";
+            default: return ``;
+        }
+    }
+
     static async CreateRepository(dto: RepoCreateDTO): Promise<AxiosResponse<RepoDTO>> {
         return await axiosInstance.post(`/repositories`, dto);
     }
--- client/src/pages/RepoOfUser.tsx
+++ client/src/pages/RepoOfUser.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate, useNavigation, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Card, Row, Col, Spinner, Button } from 'react-bootstrap';
-import { RepoDTO, RepositoryService, ReposOfUserDTO } from '../api/repo.api';
+import { RepoDTO, RepositoryBadge, RepositoryService, ReposOfUserDTO } from '../api/repo.api';
 import { AxiosError, AxiosResponse } from 'axios';
 import './RepoOfUser.css';
 import { getJwtId } from '../util/localstorage';
+import { formatDistanceToNow } from 'date-fns';
 
 export const RepositoriesOfUser: React.FC = () => {
     const [fullResult, setFullResult] = useState<ReposOfUserDTO>();
@@ -22,6 +23,10 @@ export const RepositoriesOfUser: React.FC = () => {
     const [selectedOrg, setSelectedOrg] = useState<number | undefined>(undefined);
     const [showPublic, setShowPublic] = useState(true);
     const [showPrivate, setShowPrivate] = useState(true);
+
+    const [showBadgeOfficial, setShowBadgeOfficial] = useState(true);
+    const [showBadgeVerified, setShowBadgeVerified] = useState(true);
+    const [showBadgeSponsoredOSS, setShowBadgeSponsoredOSS] = useState(true);
 
     let navigate = useNavigate();
 
@@ -64,30 +69,75 @@ export const RepositoriesOfUser: React.FC = () => {
     const handleOrgChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const orgId = Number(e.target.value);
         setSelectedOrg(orgId);
-        filterRepos(searchTerm, orgId, showPublic, showPrivate);
+        filterRepos(searchTerm, orgId, showPublic, showPrivate, showBadgeOfficial, showBadgeVerified, showBadgeSponsoredOSS);
     };
 
     const handlePublicChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setShowPublic(e.target.checked);
-        filterRepos(searchTerm, selectedOrg, e.target.checked, showPrivate);
+        filterRepos(searchTerm, selectedOrg, e.target.checked, showPrivate, showBadgeOfficial, showBadgeVerified, showBadgeSponsoredOSS);
     };
 
     const handlePrivateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setShowPrivate(e.target.checked);
-        filterRepos(searchTerm, selectedOrg, showPublic, e.target.checked);
+        filterRepos(searchTerm, selectedOrg, showPublic, e.target.checked, showBadgeOfficial, showBadgeVerified, showBadgeSponsoredOSS);
     };
 
-    const filterRepos = (searchTerm: string, orgId?: number, showPublic?: boolean, showPrivate?: boolean) => {
+    const handleBadgeOfficialChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setShowBadgeOfficial(e.target.checked);
+        filterRepos(searchTerm, selectedOrg, showPublic, showPrivate, e.target.checked, showBadgeVerified, showBadgeSponsoredOSS);
+    };
+
+    const handleBadgeVerifiedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setShowBadgeVerified(e.target.checked);
+        filterRepos(searchTerm, selectedOrg, showPublic, showPrivate, showBadgeOfficial, e.target.checked, showBadgeSponsoredOSS);
+    };
+
+    const handleBadgeSponsoredOSSChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setShowBadgeSponsoredOSS(e.target.checked);
+        filterRepos(searchTerm, selectedOrg, showPublic, showPrivate, showBadgeOfficial, showBadgeVerified, e.target.checked);
+    };
+
+    const filterRepos = (searchTerm: string, orgId?: number, showPublic?: boolean, showPrivate?: boolean, showBadgeOfficial?: boolean, showBadgeVerified?: boolean, showBadgeSponsoredOSS?: boolean) => {
         let filtered = repositories.filter((repo) => {
             const matchesSearch =
                 repo.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
                 (repo.desc && repo.desc.toLowerCase().includes(searchTerm.toLowerCase()));
             const matchesOrg = orgId ? repo.organization_id === orgId : true;
             const matchesVisibility = (showPublic && repo.public) || (showPrivate && !repo.public);
-            return matchesSearch && matchesOrg && matchesVisibility;
+            const matchesBadge =
+                (repo.badge == RepositoryBadge.none)
+                || (repo.badge == RepositoryBadge.official && showBadgeOfficial)
+                || (repo.badge == RepositoryBadge.verified && showBadgeVerified)
+                || (repo.badge == RepositoryBadge.sponsored_oss && showBadgeSponsoredOSS);
+            return matchesSearch && matchesOrg && matchesVisibility && matchesBadge;
         });
         setFilteredRepos(filtered);
     };
+
+    // Respecting DRY.
+    // TODO: `checked` stores reactive state, but `badgeDataBundle` is a regular array,
+    // so this doesn't actually work. However, we filter inside a filter() and the checkboxes
+    // are always in sync, so this sort of works by accident.
+    const badgeDataBundle = [
+        {
+            type: RepositoryBadge.official,
+            checked: showBadgeOfficial,
+            onChange: handleBadgeOfficialChange,
+            id: "officialCheckbox",
+        },
+        {
+            type: RepositoryBadge.verified,
+            checked: showBadgeVerified,
+            onChange: handleBadgeVerifiedChange,
+            id: "verifiedCheckbox",
+        },
+        {
+            type: RepositoryBadge.sponsored_oss,
+            checked: showBadgeSponsoredOSS,
+            onChange: handleBadgeSponsoredOSSChange,
+            id: "sponsoredOSSCeckbox",
+        },
+    ];
 
 
     if (loading) {
@@ -168,52 +218,106 @@ export const RepositoriesOfUser: React.FC = () => {
                             </label>
                         </div>
                     </div>
-                </div>
-            )}
 
-            {filteredRepos.map((repo) => (
-                <Col key={repo.id} xs={12}>
-                    <Card>
-                        <Card.Body>
-                            {/* Repository Title with React Router Link */}
-                            <Card.Title>
-                                <Link to={`/r/${repo.canonical_name}`} className="text-primary">
-                                    <span>{repo.name}</span>
-                                </Link>
-                                {!repo.public && (
-                                    <span className="badge rounded-pill bg-secondary" style={{ fontSize: '0.7rem', marginLeft: '1em' }}>
-                                        <i className="bi bi-lock"></i>
-                                        Private
+                    {/* Badges - Checkbox for each badge type. */}
+                    {badgeDataBundle.map((badge) => (
+                        <div className="mb-3">
+                            <div className="form-check ms-2 d-flex align-items-center">
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input form-check-lg"
+                                    checked={badge.checked}
+                                    onChange={badge.onChange}
+                                    id={badge.id}
+                                />
+                                <label className="form-check-label fs-5 ms-2" htmlFor={badge.id}>
+                                    <span className={`badge rounded-pill ${RepositoryService.BadgeToBootstrapColor(badge.type)}`}>
+                                        <i className={`bi ${RepositoryService.BadgeToHumanBootstrapIcon(badge.type)}`}> </i>
+                                        {RepositoryService.BadgeToHumanText(badge.type)}
                                     </span>
+                                </label>
+                            </div>
+                        </div>
+                    ))}
+                </div >
+            )
+            }
+
+            {
+                filteredRepos.map((repo) => (
+                    <Col key={repo.id} xs={12}>
+                        <Card>
+                            <Card.Body>
+                                {/* Repository Title with React Router Link */}
+                                <Card.Title>
+                                    <Link to={`/r/${repo.canonical_name}`} className="text-primary">
+                                        <span>{repo.name}</span>
+                                    </Link>
+                                    {/* Private */}
+                                    {!repo.public && (
+                                        <span className="badge rounded-pill bg-secondary" style={{ fontSize: '0.7rem', marginLeft: '1em' }}>
+                                            <i className="bi bi-lock"></i>
+                                            Private
+                                        </span>
+                                    )}
+                                    {/* Badge */}
+                                    {repo && repo?.badge !== RepositoryBadge.none &&
+                                        <span
+                                            className={`badge rounded-pill ${RepositoryService.BadgeToBootstrapColor(repo?.badge)}`}
+                                            style={{ fontSize: '0.7rem', marginLeft: '1em' }}
+                                        >
+                                            <i className={`bi ${RepositoryService.BadgeToHumanBootstrapIcon(repo?.badge)}`}> </i>
+                                            {RepositoryService.BadgeToHumanText(repo?.badge)}
+                                        </span>
+                                    }
+                                </Card.Title>
+
+                                {/* Organization Name (if exists) */}
+                                {repo.organization_id && (
+                                    <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: '0.8rem' }}>
+                                        Part of organization {orgNames?.get(repo.organization_id)}
+                                    </Card.Subtitle>
                                 )}
-                            </Card.Title>
 
-                            {/* Organization Name (if exists) */}
-                            {repo.organization_id && (
-                                <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: '0.8rem' }}>
-                                    Part of organization {orgNames?.get(repo.organization_id)}
-                                </Card.Subtitle>
-                            )}
+                                {/* Last update */}
+                                {repo.last_updated && (
+                                    <Card.Text style={{ fontSize: '0.8rem' }}>
+                                        {formatDistanceToNow(new Date(repo.last_updated), { addSuffix: true })}
+                                    </Card.Text>
+                                )}
 
-                            {/* Description */}
-                            {repo.desc && (
-                                <Card.Text style={{ fontSize: '0.9rem' }}>
-                                    {repo.desc}
-                                </Card.Text>
-                            )}
+                                {/* Description */}
+                                {repo.desc && (
+                                    <Card.Text style={{ fontSize: '0.9rem' }}>
+                                        {repo.desc}
+                                    </Card.Text>
+                                )}
 
-                            {/* Star Count [TODO] */}
-                            {/*repo.stars > 0*/ true && (
-                                <div className="d-flex align-items-center">
-                                    <i className="bi bi-moon moon"></i>
-                                    <span>{17}</span>
-                                    {/* <span>{repo.stars}</span> */}
+                                <div className="d-flex">
+                                    {/* Download Count */}
+                                    {
+                                        <span className="align-items-center">
+                                            <i className="bi bi-download"></i>
+                                            <span> {repo.downloads}</span>
+                                        </span>
+                                    }
+
+                                    <i className="bi bi-dot" style={{ marginLeft: '0.2em', marginRight: '0.2em' }}></i>
+
+                                    {/* Star Count [TODO] */}
+                                    {/*repo.stars > 0*/ true && (
+                                        <span className="align-items-center">
+                                            <i className="bi bi-moon moon"></i>
+                                            <span>{17}</span>
+                                            {/* <span>{repo.stars}</span> */}
+                                        </span>
+                                    )}
                                 </div>
-                            )}
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))
+            }
+        </Row >
     );
 };
