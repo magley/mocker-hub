eb35d3738c26bf9517e0602c36742116228fba69
magley <60552482+magley@users.noreply.github.com>
2024-12-28 14:35:54 +0100

    feat: Perform access control on push/pull to docker registry

--- server/app/api/registry/registry_controller.py
+++ server/app/api/registry/registry_controller.py
@@ -2,23 +2,24 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 
 from app.api.user.user_service import UserService, get_user_service
 from app.api.registry.registry_utils import decode_auth_header, parse_scope, build_jwt_for_docker_registry
+from app.api.access_control.access_control_service import AccessControlService, get_access_control_service
+from app.api.repo.repo_service import RepositoryService, get_repo_service
+from app.api.registry.registry_dto import RegistryActionOperation
 
 router = APIRouter(prefix="/registry", tags=["dockerhub-registry"])
 
-# In case you still have issues, create a file `/etc/docker/daemon.json`
-# and write the following:
-# 
-# {
-#  "insecure-registries": ["localhost:5000"]
-# }
-
 @router.get("", summary="???")
-def registry_endpoint(request: Request, user_service: UserService = Depends(get_user_service), service: str | None = None, scope: str | None = None):
+def registry_endpoint(
+    request: Request, 
+    user_service: UserService = Depends(get_user_service), 
+    repo_service: RepositoryService = Depends(get_repo_service),
+    access_control_service: AccessControlService = Depends(get_access_control_service),
+    service: str | None = None, scope: str | None = None
+    ):
     authorization_header = request.headers.get("Authorization")
     print(authorization_header)
 
     if not authorization_header:
-        # TODO:  Most `pull` operations _should_ work (unless the repo is private etc.)
         raise HTTPException(status_code=401, detail="Authorization header missing")
 
     if not authorization_header.startswith("Basic "):
@@ -30,14 +31,24 @@ def registry_endpoint(request: Request, user_service: UserService = Depends(get_
     if not user_service.exists_with_credentials(username, password):
         raise HTTPException(status_code=401, detail="Invalid username or password")
 
-    if scope is not None:
+    if scope is not None:  # Push/pull to repo. If it's None, then this is a login.
         action = parse_scope(username, scope)
-        print(action)
+        user = user_service.find_by_username(username)
+        repo = repo_service.find_by_canonical_name(action.repo_canonical_name)
 
-        raise HTTPException(status_code=401)
+        if RegistryActionOperation.push in action.operations:
+            # TODO: Once we have has_write_access, use it here.
+            can_write = access_control_service.has_read_access(user.id, repo.id)
+            if not can_write:
+                raise HTTPException(status_code=401, detail=f"User {user.username} cannot push to repo {repo.canonical_name}")
+        elif RegistryActionOperation.pull in action.operations:
+            can_read = access_control_service.has_read_access(user.id, repo.id)
+            if not can_read:
+                raise HTTPException(status_code=401, detail=f"User {user.username} cannot pull from repo {repo.canonical_name}")
+        else:
+            raise HTTPException(status_code=400, detail=f"Unknown operations {action.operations}")
 
     jwt = build_jwt_for_docker_registry(username, service, scope)
-
     return {"token": jwt}
 
 
