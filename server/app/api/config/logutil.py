a5da389df9aaef8f8a34e2415967f149f3f8c785
magley <60552482+magley@users.noreply.github.com>
2025-01-11 16:56:12 +0100

    fix: Fix backend log crashing during tests

--- server/app/api/config/logutil.py
+++ server/app/api/config/logutil.py
@@ -1,5 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 import sys
 
 LOGGER = logging.getLogger()  # If later on we want to ignore the default log messages, we can add a name here...
@@ -11,14 +12,17 @@ backup_count = 3
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-# Log to file.
+# TODO: We probably want to include logging to file in integration tests, should
+# we ever test ES<->Server. I'm not sure how to do that though.
+if os.getenv('mocker_hub_TEST_ENV') is None:
+    # Log to file.
 
-rotating_handler = RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
-rotating_handler.setFormatter(formatter)
-LOGGER.addHandler(rotating_handler)
+    rotating_handler = RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
+    rotating_handler.setFormatter(formatter)
+    LOGGER.addHandler(rotating_handler)
 
-# Log to stdout.
+    # Log to stdout.
 
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-LOGGER.addHandler(console_handler)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    LOGGER.addHandler(console_handler)
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,3 +2,19 @@
 
 echo "Starting FastAPI app..."
 fastapi run app/api/main.py --port 8000 --proxy-headers
+
+
+# if [ "$RUN_TESTS" = "true" ]; then
+#   echo "Running tests..."
+#   pytest --maxfail=1 --disable-warnings --tb=short
+
+#   if [ $? -eq 0 ]; then
+#     echo "Tests passed successfully!"
+#   else
+#     echo "Tests failed!"
+#     exit 1
+#   fi
+# else
+#   echo "Starting FastAPI app..."
+#   fastapi run app/api/main.py --port 8000 --proxy-headers
+# fi
