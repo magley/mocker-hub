48141b88715e3659b7f3a1b5fd61b8a2ada15daf
magley <60552482+magley@users.noreply.github.com>
2025-01-11 17:27:53 +0100

    test: Run tests through docker compose
    Seems to work for now (without testing anything related to ES).

--- server/app/tests/README.md
+++ server/app/tests/README.md
@@ -1,5 +1,27 @@
 ## Running the tests
 
+To run tests, do
+```sh
+RUN_TESTS=true docker compose up --build
+```
+
+However, I've been having issues with this on WSL,
+so instead I've created an `.env` in project root:
+
+```sh
+RUN_TESTS=true
+```
+
+And then `docker compose up --build`.
+
+When you're done testing, remove the ENV variable.
+
+---
+---
+---
+---
+## Old (TODO: Remove)
+
 1) Position yourself to the root of this repository. We need to do this because of ./volume-server-cfg.
 
 ```sh
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-echo "Starting FastAPI app..."
-fastapi run app/api/main.py --port 8000 --proxy-headers
+echo "RUN_TESTS: $RUN_TESTS"  # Debugging line
 
+if [ "$RUN_TESTS" = "true" ]; then
+  echo "Running tests..."
+  ls -l
+  mocker_hub_TEST_ENV=1 pytest /code/app/tests -s --maxfail=1 --disable-warnings --tb=short
 
-# if [ "$RUN_TESTS" = "true" ]; then
-#   echo "Running tests..."
-#   pytest --maxfail=1 --disable-warnings --tb=short
-
-#   if [ $? -eq 0 ]; then
-#     echo "Tests passed successfully!"
-#   else
-#     echo "Tests failed!"
-#     exit 1
-#   fi
-# else
-#   echo "Starting FastAPI app..."
-#   fastapi run app/api/main.py --port 8000 --proxy-headers
-# fi
+  if [ $? -eq 0 ]; then
+    echo "Tests passed successfully!"
+    exit 0
+  else
+    echo "Tests failed!"
+    exit 1
+  fi
+else
+  echo "Starting FastAPI app..."
+  fastapi run app/api/main.py --port 8000 --proxy-headers
+fi
