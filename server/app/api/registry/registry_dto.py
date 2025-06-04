568b9019a5626781ad95ff527a04e8466ca75dd6
Vasilije <vasilijezzzz@gmail.com>
2025-06-04 03:54:21 +0200

    feat: Update DTO

--- server/app/api/registry/registry_dto.py
+++ server/app/api/registry/registry_dto.py
@@ -5,8 +5,13 @@ from pydantic import BaseModel
 class RegistryActionOperation(str, enum.Enum):
     pull = "pull"
     push = "push"
+    delete = "delete"
 
 class RegistryAction(BaseModel):
     username: str
     repo_canonical_name: str
     operations: List[RegistryActionOperation]
+
+class DeleteRepoTagDTO(BaseModel):
+    repo_id: int
+    tag_name: str
