e6f756393bf2ac2bcbc451645e70adf07e2f2ddb
Vasilije <vasilijezzzz@gmail.com>
2025-06-13 03:59:22 +0200

    feat: Add `Delete tag` button

--- client/src/api/tags.api.ts
+++ client/src/api/tags.api.ts
@@ -9,6 +9,15 @@ export interface TagDTO {
     last_pushed_by_username: string,
 }
 
+export interface DeleteTagDTO {
+    repo_id: number,
+    tag_name: string
+}
+
+export interface DeleteTagResponseDTO {
+    message: string
+}
+
 export class TagsService {
     static async GetAllTagsOfRepo(repo_canonical_name: string): Promise<AxiosResponse<TagDTO[]>> {
         return await axiosInstance.get(`/tags/?repo_name=${repo_canonical_name}`);
@@ -20,5 +29,10 @@ export class TagsService {
         queryParams.append('search_query', search_query);
 
         return await axiosInstance.get(`/tags/filter?${queryParams.toString()}`);
+    }
+
+
+    static async DeleteTag(dto: DeleteTagDTO): Promise<AxiosResponse<DeleteTagResponseDTO>> {
+        return await axiosInstance.delete(`/registry/tag`, { data: dto });
     }
 }
