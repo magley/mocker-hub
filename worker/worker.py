0afea1d37334d0786106303a068bac6b93ff3eec
Vasilije <vasilijezzzz@gmail.com>
2025-06-21 21:28:10 +0200

    feat: Add `worker` component

---
+++ worker/worker.py
@@ -0,0 +1,13 @@
+import os
+from redis import Redis
+from rq import Worker, Queue
+
+scheme = os.environ["REDIS_SCHEME"]
+host = os.environ["REDIS_HOST"]
+port = os.environ["REDIS_PORT"]
+connection = Redis.from_url(f"{scheme}://{host}:{port}")
+
+if __name__ == '__main__':
+    default_queue = [Queue(name, connection=connection) for name in ['default']]
+    worker = Worker(queues=default_queue, connection=connection)
+    worker.work()
