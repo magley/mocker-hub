40fe289025881b629cf8bb464e9d60c99c09a0d2
magley <60552482+magley@users.noreply.github.com>
2025-07-19 10:04:22 +0200

    feat: Show proper user registration error on the web app

--- client/src/pages/UserRegistration.tsx
+++ client/src/pages/UserRegistration.tsx
@@ -43,7 +43,20 @@ export const UserRegistration = () => {
         UserService.RegisterRegularUser(dto).then(() => {
             navigate("/login");
         }).catch((err: AxiosError) => {
-            setError((err.response?.data as any)["detail"]["message"]);
+            try {
+                let err_msg = (err.response?.data as any)["detail"]["message"];
+                if (Array.isArray(err_msg)) {
+                    setError(err_msg[0]["msg"]);
+                } else if (typeof err_msg == "string") {
+                    setError(err_msg);
+                } else {
+                    setError((err.response?.data as any)["detail"]["message"]);
+                }
+            } catch (e) {
+                setError("Invalid user input");
+                console.error(e);
+                console.error(err.response?.data as any);
+            }
         });
     };
 
