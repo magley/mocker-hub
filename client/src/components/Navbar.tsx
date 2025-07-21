aff7f47bad188118d73e53ff1b337b48b71aa268
Hristina Adamovic <hristinacina@gmail.com>
2025-07-21 23:02:29 +0200

    feat: navbar improvements

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,28 @@
 import { NavLink } from "react-router";
 import "./Navbar.css";
 import { useAuthStore } from "../util/store";
+import { getJwtUsername } from "../util/localstorage";
+import { useState, useRef } from "react";
+import { useClickAway } from "react-use";
 
 export function Navbar() {
     const role = useAuthStore((state) => state.role); // "" if no JWT.
+    const username = getJwtUsername(); 
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
+    const handleDropdownClick = () => setShowDropdown(false);
+
+    useClickAway(dropdownRef, () => {
+        setShowDropdown(false);
+    });
 
     return (
-        <nav>
+        <nav className="navbar-container">
+            {/* Left */}
             <NavLink className={"navlink bold"} to="/" end><span className="white">Mocker</span><span className="blue">Hub</span></NavLink>
+            
+            {/* Center*/}
+            <div className="navbar-center">
             {
                 role === '' &&
                 <>
@@ -16,27 +31,46 @@ export function Navbar() {
                 </>
             }
             {
-                role === 'user' &&
-                <>
-                    <NavLink className={"navlink"} to="/new" end>New repo</NavLink>
-                </>
-            }
-            {
-                role === 'admin' &&
-                <>
-                    <NavLink className={"navlink"} to="/new" end>New repo</NavLink>
-                </>
-            }
-            {
                 role === 'superadmin' &&
                 <>
                     <NavLink className={"navlink"} to="/register-admin" end>New admin</NavLink>
+
                 </>
             }
             {
                 role !== '' &&
                 <>
-                    <NavLink className={"navlink"} to="/logout" end>Log out</NavLink>
+                    <NavLink className={"navlink"} to="/." end>Explore</NavLink>
+                    <NavLink className={"navlink"} to={`/u/${username}/repos`} end>Repositories</NavLink>
+                    <NavLink className={"navlink"} to={`/u/${username}/orgs`} end>Organisations</NavLink>
+                </>
+            }
+                        {
+                (role === 'admin' || role === 'superadmin') &&
+                <>
+                    <NavLink className={"navlink"} to="" end>Users</NavLink>
+                    <NavLink className={"navlink"} to="" end>Analytics</NavLink>
+                </>
+            }
+            </div>
+
+            {/* Right: Account Dropdown */}
+            {
+                role !== '' &&
+                <>
+                <div className="navbar-right" ref={dropdownRef}>
+                <span className="username"  onClick={() => setShowDropdown(!showDropdown)}>
+                    {username} </span>
+                    {showDropdown && (
+                        <div className="dropdown-menu">
+                            <NavLink to={`/u/${username}/profile`} onClick={handleDropdownClick}>Profile</NavLink>
+                            {role === "user" && (
+                            <NavLink to={`/u/${username}/starred`} onClick={handleDropdownClick}>Starred Repositories</NavLink>
+                            )}
+                            <NavLink to="/logout" onClick={handleDropdownClick}>Log out</NavLink>
+                        </div>
+                    )}
+                </div>
                 </>
             }
         </nav>
