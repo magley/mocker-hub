a085fc3370ae6683c9a789922aa5756e49b0a727
Hristina Adamovic <hristinacina@gmail.com>
2025-07-23 13:53:08 +0200

    fix: new navbar layout and icons

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -24,6 +24,32 @@ export function Navbar() {
             {/* Center*/}
             <div className="navbar-center">
             {
+                role === 'superadmin' &&
+                <>
+                    <NavLink className={"navlink"} to="/register-admin" end><i className="bi bi-person-plus"></i>New admin</NavLink>
+
+                </>
+            }
+            {
+                role !== '' &&
+                <>
+                    <NavLink className={"navlink"} to="/." end><i className="bi bi-search"></i>Explore</NavLink>
+                    <NavLink className={"navlink"} to={`/u/${username}/repos`} end><i className="bi bi-collection"></i>Repositories</NavLink>
+                    <NavLink className={"navlink"} to={`/u/${username}/orgs`} end><i className="bi bi-building"></i>Organisations</NavLink>
+                </>
+            }
+                        {
+                (role === 'admin' || role === 'superadmin') &&
+                <>
+                    <NavLink className={"navlink"} to="" end><i className="bi bi-people"></i>Users</NavLink>
+                    <NavLink className={"navlink"} to="" end><i className="bi bi-graph-up"></i>Analytics</NavLink>
+                </>
+            }
+            </div>
+
+            {/* Right */}
+            <div className="navbar-right" ref={dropdownRef}>
+            {
                 role === '' &&
                 <>
                     <NavLink className={"navlink"} to="/login" end>Log in</NavLink>
@@ -31,48 +57,21 @@ export function Navbar() {
                 </>
             }
             {
-                role === 'superadmin' &&
-                <>
-                    <NavLink className={"navlink"} to="/register-admin" end>New admin</NavLink>
-
-                </>
-            }
-            {
                 role !== '' &&
                 <>
-                    <NavLink className={"navlink"} to="/." end>Explore</NavLink>
-                    <NavLink className={"navlink"} to={`/u/${username}/repos`} end>Repositories</NavLink>
-                    <NavLink className={"navlink"} to={`/u/${username}/orgs`} end>Organisations</NavLink>
-                </>
-            }
-                        {
-                (role === 'admin' || role === 'superadmin') &&
-                <>
-                    <NavLink className={"navlink"} to="" end>Users</NavLink>
-                    <NavLink className={"navlink"} to="" end>Analytics</NavLink>
+                    <span className="username"  onClick={() => setShowDropdown(!showDropdown)}>{username} <i className="bi-caret-down-fill"></i> </span>
+                    {showDropdown && (
+                        <div className="dropdown-menu">
+                            <NavLink to={`/u/${username}/profile`} onClick={handleDropdownClick}><i className="bi bi-person"></i>Profile</NavLink>
+                            {role === "user" && (
+                            <NavLink to={`/u/${username}/starred`} onClick={handleDropdownClick}><i className="bi bi-star"></i>Starred Repositories</NavLink>
+                            )}
+                            <NavLink to="/logout" onClick={handleDropdownClick}><i className="bi bi-box-arrow-right"></i>Log out</NavLink>
+                        </div>
+                    )}
                 </>
             }
             </div>
-
-            {/* Right: Account Dropdown */}
-            {
-                role !== '' &&
-                <>
-                <div className="navbar-right" ref={dropdownRef}>
-                <span className="username"  onClick={() => setShowDropdown(!showDropdown)}>
-                    {username} </span>
-                    {showDropdown && (
-                        <div className="dropdown-menu">
-                            <NavLink to={`/u/${username}/profile`} onClick={handleDropdownClick}>Profile</NavLink>
-                            {role === "user" && (
-                            <NavLink to={`/u/${username}/starred`} onClick={handleDropdownClick}>Starred Repositories</NavLink>
-                            )}
-                            <NavLink to="/logout" onClick={handleDropdownClick}>Log out</NavLink>
-                        </div>
-                    )}
-                </div>
-                </>
-            }
         </nav>
     );
 }
