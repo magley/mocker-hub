d129eaaca5027fce4e9b588c2e0893d31a7bc775
Hristina Adamovic <hristinacina@gmail.com>
2025-07-25 18:22:39 +0200

    test: Write unit tests for organization memebers

--- server/app/tests/test_organization.py
+++ server/app/tests/test_organization.py
@@ -15,7 +15,7 @@ from app.api.user.user_model import User
 def org_service():
     session = MagicMock()
     service = OrganizationService(session)
-    
+
     service.org_repo = MagicMock()
     service.add_user_to_org = MagicMock()
     return service
@@ -137,6 +137,7 @@ def test_find_org_names_by_ids_no_matches(org_service):
     assert result == expected_output
     org_service.org_repo.find_orgs_by_ids.assert_called_once_with(ids)
 
+
 class TestGetOrgNamesFromRepos:
     ''' 
     The following tests for `get_org_names_from_repos` are trivial,
@@ -177,8 +178,9 @@ class TestGetOrgNamesFromRepos:
         assert result[o2.id] == o2.name
         org_service.org_repo.find_orgs_by_ids.assert_called_once_with(ids)
 
+
 class TestUpdateOrgAttrs():
-        
+
     def test_org_not_exist(self, org_service):
         """ Test case for when the org does not exist. """
         org_name = "o1"
@@ -186,11 +188,11 @@ class TestUpdateOrgAttrs():
 
         with pytest.raises(NotFoundException):
             org_service.update_org_attrs(org_name, desc="desc123")
-        
+
         org_service.org_repo.find_by_name.assert_called_once_with(org_name)
 
     def test_attributes_not_passed(self, org_service):
-        """ Test case for when no attributes are passed. """ 
+        """ Test case for when no attributes are passed. """
         org = MagicMock(spec=Organization)
         org.id = 1
         org.name = "o1"
@@ -199,14 +201,14 @@ class TestUpdateOrgAttrs():
 
         result = org_service.update_org_attrs(org.name)
 
-        assert result == org 
+        assert result == org
         assert result.id == org.id
         assert result.name == org.name
         assert result.desc == org.desc
         org_service.org_repo.find_by_name.assert_called_once_with(org.name)
 
     def test_attribute_not_exist(self, org_service):
-        """ Test case for when a specified attribute does not exist. """  
+        """ Test case for when a specified attribute does not exist. """
         org = MagicMock(spec=Organization)
         org.id = 1
         org.name = "o1"
@@ -214,13 +216,13 @@ class TestUpdateOrgAttrs():
         org_service.org_repo.find_by_name.return_value = org
         org_service.org_repo.set_attribute.side_effect = ValueError("unknown attribute")
 
-        with pytest.raises(ValueError) as e: 
+        with pytest.raises(ValueError) as e:
             org_service.update_org_attrs(org.name, unknown_attr="fail")
-        
+
         assert "unknown attribute" in str(e.value)
         org_service.org_repo.find_by_name.assert_called_once_with(org.name)
         org_service.org_repo.set_attribute.assert_called_once_with(org, "unknown_attr", "fail")
-    
+
     def test_successfully_update_attributes(self, org_service):
         """ Test case for when the attributes exist. """
         org = MagicMock(spec=Organization)
@@ -232,6 +234,7 @@ class TestUpdateOrgAttrs():
         def mock_set_attribute(org, attr, value):
             setattr(org, attr, value)
             return org
+
         org_service.org_repo.set_attribute.side_effect = mock_set_attribute
 
         result = org_service.update_org_attrs(name=org.name, deleting=True, desc="new_desc")
@@ -241,10 +244,11 @@ class TestUpdateOrgAttrs():
         assert result.desc == "new_desc"
         org_service.org_repo.find_by_name.assert_called_once_with(org.name)
         expected_calls = [
-            call(org, "deleting", True), 
-            call(org, "desc", "new_desc"), 
+            call(org, "deleting", True),
+            call(org, "desc", "new_desc"),
         ]
         assert org_service.org_repo.set_attribute.call_args_list == expected_calls
+
 
 class TestFindByName:
 
@@ -253,7 +257,7 @@ class TestFindByName:
         org_name = "o999"
         org_service.org_repo.find_by_name.return_value = None
 
-        with pytest.raises(NotFoundException): 
+        with pytest.raises(NotFoundException):
             org_service.find_by_name(org_name)
 
         org_service.org_repo.find_by_name.assert_called_once_with(org_name)
@@ -269,10 +273,11 @@ class TestFindByName:
         org_service.org_repo.find_by_name.assert_called_once_with(org.name)
         assert result == org
 
+
 class TestUpdateOrgDescByName:
 
     def test_org_not_exist(self, org_service):
-        """ Test case for when organization does not exist. """   
+        """ Test case for when organization does not exist. """
         user_id = 1
         dto = OrganizationDescUpdateDTO(desc="new desc")
         org_name = "o1"
@@ -285,7 +290,7 @@ class TestUpdateOrgDescByName:
         org_service.org_repo.find_by_name.assert_called_once_with(org_name)
 
     def test_non_eligible_user(self, org_service):
-        """ Test case for when the user is not eligible to make an update. """   
+        """ Test case for when the user is not eligible to make an update. """
         user_id = 1
         dto = OrganizationDescUpdateDTO(desc="new desc")
 
@@ -293,7 +298,7 @@ class TestUpdateOrgDescByName:
         owner.id = 999999
         org = MagicMock(spec=Organization)
         org.name = "o1"
-        org.owner = owner        
+        org.owner = owner
 
         org_service.org_repo.find_by_name.return_value = org
 
@@ -304,7 +309,7 @@ class TestUpdateOrgDescByName:
         org_service.org_repo.find_by_name.assert_called_once_with(org.name)
 
     def test_successful_update(self, org_service):
-        """ Test case for when the user is eligible and organization does exist. """   
+        """ Test case for when the user is eligible and organization does exist. """
         dto = OrganizationDescUpdateDTO(desc="new desc")
         user = MagicMock(spec=User)
         user.id = 1
@@ -337,7 +342,7 @@ class TestUpdateOrgDescByName:
                 }
                 response = client.post("/api/v1/users/", json=data)
                 return response.json()
-            
+
             def log_in(username: str, password: str = "12345678"):
                 data = {
                     "username": username,
@@ -348,7 +353,7 @@ class TestUpdateOrgDescByName:
                     assert response.json() == False
                 jwt = response.json()["token"]
                 return jwt
-            
+
             def change_superadmin_password():
                 # [1] Loading the super admin credentials
                 config_file = "./volume-server-cfg/superadmin_password.txt"
@@ -366,7 +371,7 @@ class TestUpdateOrgDescByName:
                 }
                 response = client.post("/api/v1/users/password", json=data, headers=header)
                 assert response.is_success
-            
+
             def add_admin(admin_username: str) -> dict:
                 jwt = log_in("admin", "12345678")
                 header = {"Authorization": f"Bearer {jwt}"}
@@ -395,7 +400,7 @@ class TestUpdateOrgDescByName:
             def update_desc(username: str, org_name: str, desc: str):
                 jwt = log_in(username)
                 header = {"Authorization": f"Bearer {jwt}"}
-                
+
                 data = {
                     "desc": desc
                 }
@@ -412,10 +417,152 @@ class TestUpdateOrgDescByName:
                 add_admin("u2")
 
             o1 = add_org("u1", "o1")
-           
+
             # User who is the organization owner
             assert update_desc("u1", o1["name"], "new desc").is_success
             # User who is trying to update a non existing organization
             assert update_desc("u1", "unknown org", "new desc").status_code == 400
             # User who is not an organization owner
             assert update_desc("u2", o1["id"], "new desc").status_code == 400
+
+
+class TestFindMembersOfOrg:
+    def test_non_eligible_user(self, org_service):
+        """ Test case for when user is not in organization """
+        org_id = 1
+        user_id = 5
+
+        org_service.org_repo.user_is_in_org.return_value = False
+
+        with pytest.raises(AccessDeniedException):
+            org_service.find_members_of_org(org_id, user_id)
+
+        org_service.org_repo.user_is_in_org.assert_called_once_with(user_id, org_id)
+        org_service.org_repo.find_members_of_org.assert_not_called()
+
+    def test_successfully_find_members(self, org_service):
+        """ Test case for when user is in organization and therefore the organization has at least one member"""
+        org_id = 1
+        user_id = 5
+
+        member1 = MagicMock(spec=User)
+        expected_members = [member1]
+
+        org_service.org_repo.user_is_in_org.return_value = True
+        org_service.org_repo.find_members_of_org.return_value = expected_members
+
+        result = org_service.find_members_of_org(org_id, user_id)
+
+        org_service.org_repo.user_is_in_org.assert_called_once_with(user_id, org_id)
+        org_service.org_repo.find_members_of_org.assert_called_once_with(org_id)
+        assert result == expected_members
+
+
+class TestAddMembersToOrg:
+    def test_org_not_exists(self, org_service):
+        """ Test case for when organization does not exist"""
+        org_id = 1
+        owner_id = 10
+        new_members_ids = [2, 3]
+
+        org_service.org_repo.find_by_id.return_value = None
+
+        with pytest.raises(NotFoundException):
+            org_service.add_members_to_org(org_id, new_members_ids, owner_id)
+
+        org_service.org_repo.find_by_id.assert_called_once_with(org_id)
+        org_service.org_repo.add_user_to_org.assert_not_called()
+
+    def test_user_is_not_owner_of_org(self, org_service):
+        """ Test case for when user is not the owner of the organization"""
+        owner_id = 10
+        new_members_ids = [2, 3]
+        org = MagicMock(spec=Organization)
+        org.id = 1
+        org.owner_id = 11
+        org_service.org_repo.find_by_id.return_value = org
+
+        with pytest.raises(AccessDeniedException):
+            org_service.add_members_to_org(org.id, new_members_ids, owner_id)
+
+        org_service.org_repo.find_by_id.assert_called_once_with(org.id)
+        org_service.org_repo.add_user_to_org.assert_not_called()
+
+    def test_skip_nonexistent_users(self, org_service):
+        """ Test case for when nonexistent users are skipped """
+        non_existent_users = [15, 16]
+        org = MagicMock(spec=Organization)
+        org.id = 1
+        org.owner_id = 10
+        org_service.org_repo.find_by_id.return_value = org
+        org_service.user_repo.find_by_id.side_effect = [None, None]
+
+        result = org_service.add_members_to_org(org.id, non_existent_users, org.owner_id)
+
+        assert result == []
+        assert org_service.user_repo.find_by_id.call_count == 2
+        org_service.org_repo.add_user_to_org.assert_not_called()
+
+    def test_skip_existing_members(self, org_service):
+        """ Test case to skip users that already are members of organization """
+        org = MagicMock(spec=Organization)
+        org.id = 1
+        org.owner_id = 10
+        org_service.org_repo.find_by_id.return_value = org
+
+        user = MagicMock(spec=User)
+        user.id = 1
+        org_service.user_repo.find_by_id.return_value = user
+        org_service.org_repo.user_is_in_org.return_value = True
+
+        result = org_service.add_members_to_org(org.id, [user.id], org.owner_id)
+
+        assert result == []
+        org_service.org_repo.add_user_to_org.assert_not_called()
+
+    def test_partial_skip(self, org_service):
+        """ Test case for when some users are added and some skipped"""
+        org = MagicMock(spec=Organization)
+        org.id = 1
+        org.owner_id = 10
+        org_service.org_repo.find_by_id.return_value = org
+
+        valid_user = MagicMock(spec=User)
+        valid_user.id = 2
+
+        superadmin = MagicMock(spec=User)
+        superadmin.id = 1
+        superadmin.role = "superadmin"
+
+        org_service.user_repo.find_by_id.side_effect = [
+            valid_user,
+            superadmin,
+            None
+        ]
+        # False for valid_user, False for superadmin, not called for non-existant user
+        org_service.org_repo.user_is_in_org.side_effect = [False, False]
+
+        result = org_service.add_members_to_org(org.id, [2, 1, 66], org.owner_id)
+        assert result == [valid_user]
+        assert org_service.org_repo.add_user_to_org.call_count == 1
+        org_service.org_repo.add_user_to_org.assert_called_once_with(org.id, 2)
+
+    def test_successfully_add_members(self, org_service):
+        """ Test case to add valid members to organization """
+        org = MagicMock(spec=Organization)
+        org.id = 1
+        org.owner_id = 10
+        org_service.org_repo.find_by_id.return_value = org
+
+        user1 = MagicMock(spec=User)
+        user1.id = 1
+        user2 = MagicMock(spec=User)
+        user2.id = 2
+
+        org_service.user_repo.find_by_id.side_effect = [user1, user2]
+        org_service.org_repo.user_is_in_org.return_value = False
+
+        result = org_service.add_members_to_org(org.id, [user1.id, user2.id], org.owner_id)
+
+        assert result == [user1, user2]
+        assert org_service.org_repo.add_user_to_org.call_count == 2
