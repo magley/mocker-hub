61fcc003add8e9de7b02279f8c6c3c008280178d
magley <60552482+magley@users.noreply.github.com>
2025-07-31 17:03:09 +0200

    test: Log search requires login

--- server/app/tests/test_z_events.py
+++ server/app/tests/test_z_events.py
@@ -8,11 +8,27 @@ def test_events___integration():
     with TestClient(app) as client:
         assert wait_for_elasticsearch()
 
-        def search_for_something():
-            response = client.get(f"/api/v1/events/?query=date_time+%3C%3D+%222165-07-24%22&page_number=1&page_size=10&sort_by=date_time&sort_ascending=true")
+        def search_for_something(headers):
+            response = client.get(
+                f"/api/v1/events/?query=date_time+%3C%3D+%222165-07-24%22&page_number=1&page_size=10&sort_by=date_time&sort_ascending=true",
+                headers=headers)
             return response
-        
-        res = search_for_something()
+
+        def login(data: dict) -> dict:
+            response = client.post("/api/v1/users/login", json=data)
+            assert response.status_code == 200
+            jwt = response.json()["token"]
+            return {"Authorization": f"Bearer {jwt}"}
+
+        superadmin_creds = {
+            "username": "admin",
+            "password": "Password1" # WARNING: Superadmin password is changed by an earlier test (update user badge?)
+            # Therefore, that must run before this!
+        }
+
+        superadmin_header = login(superadmin_creds)
+
+        res = search_for_something(superadmin_header)
         assert res.is_success
 
         hits = res.json()["hits"]
